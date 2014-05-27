daef71dad3764780384898c58b033fe5da3b9557
Tomas Aparicio <tomas@aparicio.me>
2014-05-27 23:50:35 +0200

    chore(scripts): remove unnecesary quotes in bash script

--- scripts/node.sh
+++ scripts/node.sh
@@ -2,14 +2,14 @@
 
 export PATH="$(pwd)/node_modules/.bin:${PATH}"
 
-if [ -d "$(pwd)/.node/lib/node" ]; then
+if [ -d $(pwd)/.node/lib/node ]; then
   export NODE_PATH="$(pwd)/.node/lib/node"
 fi
 
-if [ -f "$(pwd)/.node/bin/node" ]; then
+if [ -f $(pwd)/.node/bin/node ]; then
   export PATH="$(pwd)/.node/bin:${PATH}"
-  chmod +x "$(pwd)/.node/bin/node"
-  "$(pwd)/.node/bin/node" "$@"
+  chmod +x $(pwd)/.node/bin/node
+  $(pwd)/.node/bin/node $@
 else
-  node "$@"
+  node $@
 fi
