4818b80369d4dfcd06f4046442238c59cec2d1b5
Tomas Aparicio <tomas@aparicio.me>
2014-07-09 19:59:16 +0200

    feat(exec): avoid to pass the exec command to start executables binaries

--- scripts/run.sh
+++ scripts/run.sh
@@ -36,9 +36,15 @@ export PATH="${output}/bin:$PATH"
 nar_file=`cd .nar && ls *.nar | head -n 1`
 chmod +x ${output}/bin/node
 
-if [[ $command == 'exec' ]]; then
-  shift 1
-  ${output}/bin/node ${output}/nar/bin/nar start --args-start="$*" ${output}/${nar_file}
+if [[ $command == 'exec' || -z $command ]]; then
+  if [[ $command == 'exec' ]]; then 
+    shift 1
+  fi
+  if [ $# -eq 0 ]; then 
+    ${output}/bin/node ${output}/nar/bin/nar start ${output}/${nar_file}
+  else
+    ${output}/bin/node ${output}/nar/bin/nar start --args-start="$*" ${output}/${nar_file}
+  fi
 else
   ${output}/bin/node ${output}/nar/bin/nar $* ${output}/${nar_file}
 fi
